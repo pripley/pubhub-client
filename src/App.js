08f6f26eaad449be5105fd5afb8ee879d656e0c2
Paul Ripley <paulripley@hotmail.com>
2021-10-30 00:20:43 -0400

    app.js updated and component folder and files created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Sitebar from "./home/Navbar";
+import LandingPage from "./site/Landing";
+import HomePage from "./site/Main";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sessionToken: "",
+    };
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem("token")) {
+      this.setState({
+        sessionToken: localStorage.getItem("token"),
+      });
+    }
+  }
+
+  updateToken(newToken) {
+    localStorage.setItem("token", newToken);
+    this.setState({
+      sessionToken: newToken,
+    });
+  }
+
+  clearToken() {
+    localStorage.clear();
+    this.setState({
+      sessionToken: "",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Sitebar clickLogout={this.clearToken} />
+        {this.state.sessionToken === localStorage.getItem("token") ? (
+          <HomePage token={this.state.sessionToken} />
+        ) : (
+          <LandingPage updateToken={this.updateToken} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/auth/Login.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { Input, InputGroup, Row, Col, Button, Form, Container } from "reactstrap";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",      
+    };
+
+    this.email = this.email.bind(this);
+    this.password = this.password.bind(this);    
+    this.login = this.login.bind(this);
+  }
+
+  email(event) {
+    this.setState({ email: event.target.value });
+  }
+  password(event) {
+    this.setState({ password: event.target.value });
+  }
+  
+  login(event) {
+    event.preventDefault()
+    fetch("http://localhost:3000/user/login/", {
+      method: "post",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password        
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.props.updateToken(data.sessionToken)        
+      });
+  }
+
+  render() {
+    return (
+      <div className="app flex-row align-items-center">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="9" lg="7" xl="6">
+              <Form>
+                <div class="row" className="mb-2 pageheading">
+                  <div class="col-sm-12 btn btn-primary">Sign Up</div>
+                </div>                
+                <InputGroup className="mb-3">
+                  <Input
+                    type="text"
+                    onChange={this.email}
+                    placeholder="Enter Email"
+                  />
+                </InputGroup>
+                <InputGroup className="mb-3">
+                  <Input
+                    type="password"
+                    onChange={this.password}
+                    placeholder="Enter Password"
+                  />
+                </InputGroup>                
+                <Button onClick={this.login} color="success" block>
+                  Log in
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Login;
