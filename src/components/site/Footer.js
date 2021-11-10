9a94e2d932030bff6b2d0fb5bb121ee04504e8ca
Paul Ripley <paulripley@hotmail.com>
2021-11-09 20:09:33 -0500

    added container and classnames

--- src/components/site/Footer.js
+++ src/components/site/Footer.js
@@ -1,16 +1,23 @@
-import React from "react"
-import { Container, Row } from "reactstrap"
+import React from "react";
+import { Container, Row } from "reactstrap";
 
 const Footer = () => {
-    return (
-        <footer>
-            <Container>
-            <Row>
-                <p>&copy; PubHub 2020</p>
-            </Row>
-            </Container>
-        </footer>
-    )
-}
+  return (
+    <div className="footer-container">
+      <div className="responsible-message">
+        <Container>
+          <Row><p className="center">PLEASE DRINK RESPONSIBLY</p></Row>
+        </Container>
+      </div>
+      <footer>
+        <Container>
+          <Row>
+            <p>&copy; PubHub 2020</p>
+          </Row>
+        </Container>
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
