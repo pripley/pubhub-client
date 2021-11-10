56509e86d61dea3dde7129c107cc179b06484e15
Paul Ripley <paulripley@hotmail.com>
2021-11-09 20:09:03 -0500

    added Container

--- src/components/landing/LandingPage.js
+++ src/components/landing/LandingPage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import BeerOfTheDay from "./BeerOfTheDay";
 import HeroSection from "./HeroSection";
+import { Container } from "reactstrap";
 
 const LandingPage = () => {
   return (    
       <>
         <HeroSection />
+        <Container>
         <BeerOfTheDay />
+        </Container>
+        
       </>    
   );
 };
