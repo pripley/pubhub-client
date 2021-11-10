de9cedd79bb3a57b32ec9ad7942229cb0c30b077
Paul Ripley <paulripley@hotmail.com>
2021-11-09 20:04:14 -0500

    handleSaveBrewery function and added redirect

--- src/components/brewery/BrewerySave.js
+++ src/components/brewery/BrewerySave.js
@@ -1,33 +1,49 @@
 import React, { Component } from "react";
 import { Button } from "reactstrap";
+import { Redirect } from "react-router-dom";
 
 class BrewerySave extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { breweryId: 0, redirect: null };
   }
 
   // Save brewery check in
-  handleSaveBrewery = async (brewery) => {
+  handleSaveBrewery = async (e) => {
+    e.preventDefault();
+    const { brewery, token } = this.props;
+    const data = {
+      brewery: {
+        name: brewery.name,
+        street: brewery.street,
+        city: brewery.city,
+        state: brewery.state,
+        zip: brewery.postal_code,
+        type: brewery.brewery_type,
+      },
+    }    
+    // console.log(brewery.name)    
+    // console.log(brewery.street)
+    // console.log(brewery.city)
+    // console.log(brewery.state)
+    // console.log(brewery.postal_code)
+    // console.log(brewery.brewery_type)
+    console.log(token)
     try {
       const response = await fetch("http://localhost:3000/brewery/", {
-        method: "POST",
-        body: JSON.stringify({
-          brewery: {
-            name: name,
-            street: street,
-            city: city,
-            state: state,
-            zip: zip,
-            type: type,
-          },
-        }),
+        method: "POST",        
         headers: new Headers({
           "Content-Type": "application/json",
-          Authorization: props.token,
+          "Authorization": token,
         }),
+        body: JSON.stringify(data),
       });
       console.log(response);
+      const json = await response.json();
+      console.log(json);      
+      this.setState({ breweryId: json.brewery.id}); 
+      console.log(this.state.breweryId)
+      this.setState({ redirect: `/brewery/${this.state.breweryId}`}) 
       if (!response.ok) {
         throw new Error("something went wrong!");
       }
@@ -37,6 +53,11 @@ class BrewerySave extends Component {
   };
 
   render() {
+    if (this.state.redirect) {
+      return (
+        <Redirect to={{pathname: this.state.redirect, state: {breweryId: this.state.breweryId}}}/>
+      );
+    }
     return (
       <>
         <Button onClick={this.handleSaveBrewery}>Check in</Button>
