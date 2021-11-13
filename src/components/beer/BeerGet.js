94a6efc12290caf514fda7512d6f354ab203c4fa
Paul Ripley <paulripley@hotmail.com>
2021-11-13 09:31:27 -0500

    added delete and get breweries fetch

--- src/components/beer/BeerGet.js
+++ src/components/beer/BeerGet.js
@@ -1,50 +1,128 @@
 import React, { Component } from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Row, Col } from "reactstrap";
+import { Link, Redirect } from "react-router-dom";
+import BeerEdit from "./BeerEdit";
 
 class BeerGet extends Component {
   constructor(props) {
     super(props);
-    this.state = { beers: [] };
+    this.state = { beers: [], breweries: [], breweryID: 0, redirect: null };
   }
 
-  componentDidMount() {
+  handleDeleteBeer = (beer, token) => async (e) => {
+    e.preventDefault();
     const { token } = this.props;
-    (async () => {
-      try {
-        const response = await fetch("http://localhost:3000/beer/", {
-          method: "GET",
-          headers: new Headers({
-            "Content-Type": "application/json",
-            Authorization: token,
-          }),
-        });
-        console.log(response);
-        const json = await response.json();
-        this.setState({ beers: json.beers });
-        if (!response.ok) {
-          throw new Error("something went wrong!");
-        }
-      } catch (err) {
-        console.error(err);
+    try {
+      console.log(beer.id);
+      console.log(token);
+      const response = await fetch(`http://localhost:3000/beer/${beer.id}`, {
+        method: "DELETE",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Authorization: token,
+        }),
+      });
+      console.log(response);
+      this.handleFetchBeer();
+      if (!response.ok) {
+        throw new Error("something went wrong!");
       }
-    })();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  handleFetchBeer = async () => {
+    const { token } = this.props;
+    try {
+      const response = await fetch("http://localhost:3000/beer/", {
+        method: "GET",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Authorization: token,
+        }),
+      });
+      console.log(response);
+      const json = await response.json();
+      this.setState({ beers: json.beers });
+      if (!response.ok) {
+        throw new Error("something went wrong!");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  handleFetchAllBreweries = (breweryName) => async (e) => {
+    e.preventDefault();
+    console.log(breweryName);
+    const { token } = this.props;
+    try {
+      const response = await fetch("http://localhost:3000/brewery/", {
+        method: "GET",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Authorization: token,
+        }),
+      });
+      console.log(response);
+      const json = await response.json();
+      this.setState({ breweries: json.breweries });
+      if (!response.ok) {
+        throw new Error("something went wrong!");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
+    const breweries = this.state.breweries;
+    console.log(breweries);
+    breweries.forEach((brewery, index) => {
+      console.log(brewery.id);
+      this.setState({ breweryId: brewery.id });
+      brewery.name === breweryName
+        ? this.setState({ redirect: `/brewery/${brewery.id}` })
+        : this.setState({ redirect: null });
+    });
+  };
+
+  componentDidMount() {
+    this.handleFetchBeer();
   }
 
   render() {
+    if (this.state.redirect) {
+      return (
+        <Redirect
+          to={{
+            pathname: this.state.redirect,
+            state: { breweryId: this.state.breweryId },
+          }}
+        />
+      );
+    }
+    const { token } = this.props;
     const { beers } = this.state;
     console.log(beers);
     const beerList = () =>
       beers.map((beer, index) => {
         return (
-          <Container className="beer-activity">
+          <div className="beer-activity" key={index}>
             <Row>
               <Col>
-                <h4>{beer.location}</h4>
+                <h5>
+                  <Link
+                    to=""
+                    onClick={this.handleFetchAllBreweries(beer.location)}
+                  >
+                    {beer.location}
+                  </Link>
+                </h5>
               </Col>
             </Row>
             <Row>
               <Col xs="6">
-                <h5>{beer.name}</h5>
+                <h3>{beer.name}</h3>
               </Col>
               <Col xs="2">
                 <h5>Rating: {beer.rating}</h5>
@@ -52,14 +130,25 @@ class BeerGet extends Component {
               <Col xs="2">
                 <h5>{beer.servingStyle}</h5>
               </Col>
-              <Col xs="2">Update / Delete</Col>
+              <Col xs="2" className="text-end">
+                <BeerEdit
+                  beer={beer}
+                  token={token}
+                  handleFetchBeer={this.handleFetchBeer}
+                />{" "}
+                |
+                <Link to="" onClick={this.handleDeleteBeer(beer)}>
+                  {" "}
+                  Delete
+                </Link>
+              </Col>
             </Row>
             <Row>
               <Col>
                 <p>{beer.note}</p>
               </Col>
             </Row>
-          </Container>
+          </div>
         );
       });
     return <div>{beerList()}</div>;
