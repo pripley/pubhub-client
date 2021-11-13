f6a19c6d480fc0a8322466aa8ca46c57ca309884
Paul Ripley <paulripley@hotmail.com>
2021-11-13 09:27:01 -0500

    updated route for ViewUser and added  container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
-import NavBar from './components/site/NavBar';
+import NavBar from "./components/site/NavBar";
 import LandingPage from "./components/landing/LandingPage";
 import HomePage from "./components/home/HomePage";
-import SearchResults from './components/site/SearchResults';
+import SearchResults from "./components/site/SearchResults";
 import BreweryInfo from "./components/brewery/BreweryInfo";
 import ViewUser from "./components/site/ViewUser";
 import Profile from "./components/site/Profile";
-import Login from './components/auth/Login';
+import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Footer from "./components/site/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -34,40 +34,56 @@ class App extends React.Component {
     this.setState({
       sessionToken: newToken,
     });
-  }
+  };
 
   render() {
     const { sessionToken } = this.state;
     return (
       <div id="main-container">
-        <Router>
-          <NavBar/>
-          <Switch>
-            <Route exact path="/">
-              {sessionToken ? (
-                <HomePage token={sessionToken} />
-              ) : (
-                <LandingPage/>
-              )}
-            </Route>
-            <Route exact path="/brewery/:id" render={(breweryId) => <BreweryInfo {...breweryId} token={sessionToken}/>}>              
-            </Route>
-            <Route exact path="/users/:username">
-              <ViewUser token={sessionToken}/>
-            </Route>
-            <Route exact path="/search" render={(brewery) => <SearchResults {...brewery} token={sessionToken}/>}/>
-            <Route exact path="/profile">
-              <Profile />
-            </Route>
-            <Route exact path="/login">
-              <Login updateToken={this.updateToken}/>
-            </Route>
-            <Route exact path="/register">
-              <Register updateToken={this.updateToken}/>
-            </Route>
-          </Switch>
+        <div id="content-container">
+          <Router>
+            <NavBar />
+            <Switch>
+              <Route exact path="/">
+                {sessionToken ? (
+                  <HomePage token={sessionToken} />
+                ) : (
+                  <LandingPage />
+                )}
+              </Route>              
+              <Route
+                exact
+                path="/brewery/:id"
+                render={(breweryId) => (
+                  <BreweryInfo {...breweryId} token={sessionToken} />
+                )}
+              ></Route>
+              <Route exact path="/user/:id" render={(props) => (
+                <ViewUser id={props.match.params.id}/>
+              )}/>              
+              
+              <Route
+                exact
+                path="/search"
+                render={(brewery) => (
+                  <SearchResults {...brewery} token={sessionToken} />
+                )}
+              />
+              <Route exact path="/profile">
+                <Profile />
+              </Route>
+              <Route exact path="/login">
+                <Login updateToken={this.updateToken} />
+              </Route>
+              <Route exact path="/register">
+                <Register updateToken={this.updateToken} />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+        <div className="footer-container">
           <Footer />
-        </Router>
+        </div>
       </div>
     );
   }
