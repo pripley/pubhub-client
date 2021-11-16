3a1ef4b0a60106e1e12e3696ee576406a8bd8328
Paul Ripley <paulripley@hotmail.com>
2021-11-15 23:50:36 -0500

    Added greeting

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,27 +2,42 @@ import React, { Component } from "react";
 import { Container } from "reactstrap";
 import Search from "../site/Search";
 import RecentActivity from "./RecentActivity";
+import NavBar from "../site/NavBar";
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { timeOfDay: "" };
+  }
+
+  greeting = () => {
+    const hours = new Date().getHours();
+    console.log(hours);
+    hours < 12
+      ? this.setState({ timeOfDay: "Good morning!" })
+      : hours >= 12 && hours <= 18
+      ? this.setState({ timeOfDay: "Good afternoon!" })
+      : this.setState({ timeOfDay: "Good evening!" });
+  };
+
+  componentDidMount() {
+    this.greeting();
   }
 
   render() {
-    const { token } = this.props;
+    const { token, firstName } = this.props;
+    const { timeOfDay } = this.state;    
     return (
       <>
+        <NavBar token={token} firstName={firstName}/>
         <div className="search-container">
           <Container>
             <Search />
           </Container>
         </div>
         <Container>
-          <h1>Good afternoon!</h1>
+          <h1 className="mb-5">{timeOfDay}</h1>
           <RecentActivity token={token} />
-
-          {/* <h2>Beer of the Day</h2> */}
         </Container>
       </>
     );
