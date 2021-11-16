1d35d67f508b6a80700487fd46f340bb6d44a99d
Paul Ripley <paulripley@hotmail.com>
2021-11-15 23:48:24 -0500

    added brewery edit

--- src/components/brewery/BreweryInfo.js
+++ src/components/brewery/BreweryInfo.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import { Container, Button } from "reactstrap";
+import { Container, Row, Col, Button } from "reactstrap";
 import BeerSave from "../beer/BeerSave";
 import Search from "../site/Search";
 import BeerReviews from "../beer/BeerReviews";
-import APIURL from '../../helpers/environment';
+import APIURL from "../../helpers/environment";
+import NavBar from "../site/NavBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link, Redirect } from "react-router-dom";
+import BreweryEdit from './BreweryEdit';
 
 class BreweryInfo extends Component {
   constructor(props) {
     super(props);
-    this.state = { brewery: null, open: false };
+    this.state = { brewery: null, open: false, favorite: null };
     // console.log(this.props.location.state.breweryId)
   }
 
@@ -17,16 +21,13 @@ class BreweryInfo extends Component {
     console.log(token);
     console.log(breweryId);
     try {
-      const response = await fetch(
-        `${APIURL}/brewery/${breweryId}`,
-        {
-          method: "GET",
-          headers: new Headers({
-            "Content-Type": "application/json",
-            Authorization: token,
-          }),
-        }
-      );
+      const response = await fetch(`${APIURL}/brewery/${breweryId}`, {
+        method: "GET",
+        headers: new Headers({
+          "Content-Type": "application/json",
+          Authorization: token,
+        }),
+      });
       const json = await response.json();
       console.log(response);
       console.log(json);
@@ -45,32 +46,37 @@ class BreweryInfo extends Component {
 
   render() {
     const { brewery } = this.state;
-
+    const { token, firstName } = this.props;
     if (brewery === null) {
       return null;
     }
     const brewType =
       brewery.type.charAt(0).toUpperCase() + brewery.type.slice(1);
+
     return (
       <>
+        <NavBar token={token} firstName={firstName} />
         <div className="search-container">
           <Container>
             <Search />
           </Container>
         </div>
         <Container>
-          <h1>{brewery.name}</h1>
+          <Row className="mb-3 ">
+            <Col xs="auto">
+              <h1>{brewery.name}</h1>
+            </Col>
+            <Col>
+            <BreweryEdit brewery={brewery} token={token} handleFetchBrewery={this.handleFetchBrewery}/>
+            </Col>
+          </Row>
           <h3>{brewType}</h3>
           <h5>
             {brewery.street}, {brewery.city}, {brewery.state} {brewery.zip}
           </h5>
           <br />
           <BeerSave location={brewery.name} token={this.props.token} />
-          <div className="brewery-beer-activity-container">
-            <h4 className="weight-bold">Recent Check-Ins</h4>
-            <br/>
-            <BeerReviews location={brewery.name} token={this.props.token} />
-          </div>
+          <BeerReviews location={brewery.name} token={this.props.token} />
         </Container>
       </>
     );
