99924d559e947eec3d0d3d038f87d7ac609176df
Paul Ripley <paulripley@hotmail.com>
2021-11-15 23:52:57 -0500

    added login to hero section

--- src/components/landing/HeroSection.js
+++ src/components/landing/HeroSection.js
@@ -1,11 +1,31 @@
-import { Container } from "reactstrap";
+import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
 import Search from "../site/Search";
+import logo from "../../assets/images/brewpub_logo.svg";
 
 const HeroSection = (props) => {
   return (
     <div className="hero">
-      <Container className="center">
-        <Search/>
+      <Container>
+        <Row>
+          <Col>
+            <Nav className="justify-content-end mt-3">
+              <NavItem>
+                <NavLink href="/login">Login</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/register">Sign up</NavLink>
+              </NavItem>
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
+      <Container className="hero-search center">
+        <Row className="mb-5">
+          <Col>
+            <img src={logo} alt="Brewpub logo" />
+          </Col>
+        </Row>
+        <Search />
       </Container>
     </div>
   );
