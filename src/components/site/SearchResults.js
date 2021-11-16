bdf7e8f8a78338f0b2b5538e7bdf675581ecbb45
Paul Ripley <paulripley@hotmail.com>
2021-11-15 23:57:38 -0500

    added styling and updated formatting

--- src/components/site/SearchResults.js
+++ src/components/site/SearchResults.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
-import BrewerySave from '../brewery/BrewerySave';
-import Search from './Search';
+import BrewerySave from "../brewery/BrewerySave";
+import Search from "./Search";
+import NavBar from "./NavBar";
 
 class SearchResults extends Component {
   constructor(props) {
@@ -11,44 +12,59 @@ class SearchResults extends Component {
   }
 
   render() {
-    const { token } = this.props;
+    const { token, firstName } = this.props;
+    console.log(token)
+    console.log(firstName)
     const brewery = this.props.location.state.brewery;
-        
+
     const breweryList = () =>
-    brewery.map((checkedBrewery, index) => {
-      const brewType = checkedBrewery.brewery_type.charAt(0).toUpperCase() + checkedBrewery.brewery_type.slice(1)
+      brewery.map((checkedBrewery, index) => {
+        const brewType =
+          checkedBrewery.brewery_type.charAt(0).toUpperCase() +
+          checkedBrewery.brewery_type.slice(1);
         return (
           <Container className="result">
             <Row>
               <Col xs="7">
-                <h2>{checkedBrewery.name}</h2>
-                <h5>
+                <h5 className="weight-bold">{checkedBrewery.name}</h5>
+                <p>
                   {checkedBrewery.street}, {checkedBrewery.city},{" "}
                   {checkedBrewery.state} {checkedBrewery.postal_code}
-                </h5>
+                </p>
               </Col>
               <Col xs="3">
                 <h5 className="weight-bold">Brewery Type:</h5>
-                <h5>{brewType}</h5>
+                <p>{brewType}</p>
               </Col>
-              <Col xs lg="2">{token ? (<BrewerySave key={index} brewery={checkedBrewery} token={token}/>): ""}</Col>
+              <Col xs lg="2">
+                {token ? (
+                  <BrewerySave
+                    key={index}
+                    brewery={checkedBrewery}
+                    token={token}
+                  />
+                ) : (
+                  ""
+                )}
+              </Col>
             </Row>
           </Container>
         );
       });
 
     return (
-      <div>
+      <>
+        <NavBar token={token} firstName={firstName} />
         <div className="search-container">
           <Container>
             <Search />
           </Container>
-        </div> 
+        </div>
         <Container className="search-results">
           <h1>Search Results</h1>
           {breweryList()}
         </Container>
-      </div>
+      </>
     );
   }
 }
