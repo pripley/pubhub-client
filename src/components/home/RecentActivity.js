bc4c7105fcab6648dcc38e5d5c3c42c2243ad0b0
Paul Ripley <paulripley@hotmail.com>
2021-11-15 23:52:03 -0500

    added styling

--- src/components/home/RecentActivity.js
+++ src/components/home/RecentActivity.js
@@ -11,7 +11,7 @@ class RecentActivity extends Component {
     const { token } = this.props;
     return (
       <div>
-        <h2 className="title">Your Recent Activity</h2>
+        <h4 className="weight-bold mb-3">Your Recent Check-Ins</h4>
         <BeerGet token={token} />
       </div>
     );
